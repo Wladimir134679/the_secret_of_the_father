4089c1aae34df097deed660f2d9301179cc5f399
wdeath <wladimir134679@gmail.com>
2024-03-04 19:37:20 +0300

    Сдал движение гоблина

--- Mobs/goblin.gd
+++ Mobs/goblin.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 var stands = true
 var SPEED = 130
 var destination = Vector2()
-var vel = Vector2()
 var prev_pos = Vector2()
 var target = null
 
@@ -11,7 +10,7 @@ func _ready():
 	SPEED = 100
 
 func _process(delta):
-	if vel:
+	if velocity:
 		prev_pos = position
 		move_and_slide()
 		position.x = clamp(position.x, 0, 10000)
@@ -33,13 +32,13 @@ func search_for_target():
 	
 func set_destination(dest):
 	destination = dest
-	vel = (destination - position).normalized() * SPEED
+	velocity = (destination - position).normalized() * SPEED
 	stands = false
 	
 func cancel_movement():
-	vel = Vector2()
+	velocity = Vector2()
 	destination = Vector2()
-	$StandingTimer.start(2)
+	$Timer.start(2)
 
 func wander():
 	var pos = position
@@ -53,7 +52,7 @@ func wander():
 		y = clamp(y, 0, 10000)
 		
 		set_destination(Vector2(x, y))
-	elif vel != Vector2():
+	elif velocity != Vector2():
 		if pos.distance_to(destination) <= SPEED:
 			cancel_movement()
 		elif pos.distance_to(prev_pos) <= 0.6:
