96aa49a678fe52b31b2d8f990bce6651be2989ec
Gr1NeR <bagaev-deniska@mail.ru>
2024-03-23 01:55:03 +0300

    Улучшил первую сцену

--- scenes/Camera №1.gd
+++ scenes/Camera №1.gd
@@ -1,42 +1,46 @@
 extends Camera2D
 
-var pc = 0.015
-var progress = 0
-@onready var my_label = $Text2
+var pc = 0.02
+@onready var progress = 0
+@onready var my_label = $CanvasGroup/Text2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if progress == 1:
+	if progress == 0:
 		if position.x != $"../Player".position.x:
 			position.x += (($"../Player".position.x - position.x)/50)
 		if position.y != $"../Player".position.y:
 			position.y += (($"../Player".position.y - position.y)/50)
+	elif progress == 2:
+		$CanvasGroup/Text2.visible_ratio = 1.0
+		my_label.text = "Какой замечательный день, это путешествие определенно того стоило!!!"
+	elif progress == 4:
+		$CanvasGroup/Text2.visible_ratio = 1.0
+		my_label.text = "Эти троя даже не знают чего себя лишили, вот как найду могущественные артифакты ух"
 
 func _on_timer_timeout():
-	if $Text2.visible_ratio == 0.99:
-		$Timer.stop()
+	if $CanvasGroup/Text2.visible_ratio == 1.0:
+		progress += 1
+		$CanvasGroup/Text2/Timer.stop()
 	else:
-		$Text2.visible_ratio += pc
+		$CanvasGroup/Text2.visible_ratio += pc
 
 func _on_button_pressed():
 	progress += 1
 	if progress == 1:
-		$Dialog.visible = true
-		$Text2.visible_ratio = 0.0
-		my_label.text = "Какой замечательный день, это путешествие определенно того стоило!!!"
-	elif progress == 2:
-		$Text2.visible_ratio = 1.0
+		$CanvasGroup/Text2/Timer.start()
+		$CanvasGroup.visible = true
+		$CanvasGroup/Text2.visible_ratio = 0.0
 		my_label.text = "Какой замечательный день, это путешествие определенно того стоило!!!"
 	elif progress == 3:
-		$Text2.visible_ratio = 0.0
-		my_label.text = "Эти троя даже не знают чего себя лишили, вот как найду могущественные артифакты ух"
-	elif progress == 4:
-		$Text2.visible_ratio = 1.0
-		my_label.text = "Эти троя даже не знают чего себя лишили, вот как найду могущественные артифакты ух"
+		$CanvasGroup/Text2/Timer.start()
+		$CanvasGroup/Text2.visible_ratio = 0.0
+		my_label.text = "Эти троя даже не знают чего себя лишили, вот как найду могущественные артифакты и покажу им свои истиную силу"
 	elif progress == 5:
 		$"../Player".process_mode = Node.PROCESS_MODE_INHERIT
+		$"../Player/HeroBar".visible = true
 		visible = false
 		enabled = false
--- scenes/Вступление.gd
+++ scenes/Вступление.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-var pc = 0.015
-var progress = 0
+@onready var progress = 0
 @onready var my_label = $Text1
+var pc = 0.02
 
 	
 func _on_timer_timeout():
-	if $Text1.visible_ratio == 0.99:
-		$Timer.stop()
+	if $Text1.visible_ratio == 1.0:
+		progress += 1
+		$Text1/Timer.stop()
 	else:
 		$Text1.visible_ratio += pc
 
@@ -17,28 +18,33 @@ func _ready():
 	my_label.text = "Здесь будет очень много текста и когда-нибудь ахуенные диалоги:)"
 
 func _process(_delta):
-	pass
+	if progress == 1:
+		$Text1.visible_ratio = 1.0
+		my_label.text = "Здесь будет очень много текста и когда-нибудь ахуенные диалоги:)"
+	if progress == 3:
+		$Text1.visible_ratio = 1.0
+		my_label.text = "Если здесь будет другой текст, то я буду ссаться кипятком от радости"
+	elif progress == 5:
+		$Text1.visible_ratio = 1.0
+		my_label.text = "Неужто это работает и у меня получилось..."
+	elif progress == 7:
+		$Text1.visible_ratio = 1.0
+		my_label.text = "Ну раз так, тогда начнем"
 
 func _on_button_pressed():
 	progress += 1
-	if progress == 1:
+	if progress == 2:
+		$Text1/Timer.start()
 		$Text1.visible_ratio = 0.0
 		my_label.text = "Если здесь будет другой текст, то я буду ссаться кипятком от радости"
-	elif progress == 2:
-		$Text1.visible_ratio = 1.0
-		my_label.text = "Если здесь будет другой текст, то я буду ссаться кипятком от радости"
-	elif progress == 3:
-		$Text1.visible_ratio = 0.0
-		my_label.text = "Неужто это работает и у меня получилось..."
 	elif progress == 4:
-		$Text1.visible_ratio = 1.0
+		$Text1/Timer.start()
+		$Text1.visible_ratio = 0.0
 		my_label.text = "Неужто это работает и у меня получилось..."
-	elif progress == 5:
+	elif progress == 6:
+		$Text1/Timer.start()
 		$Text1.visible_ratio = 0.0
 		my_label.text = "Ну раз так, тогда начнем"
-	elif progress == 6:
-		$Text1.visible_ratio = 1.0
-		my_label.text = "Ну раз так, тогда начнем"
-	elif progress == 7:
+	elif progress == 8:
 		get_tree().change_scene_to_file("res://scenes/test_world.tscn")
 
