958215102e6c2c1d3d754b461c8c458ae6625666
wladimir134679 <wladimir134679@gmail.com>
2024-04-18 17:04:32 +0300

    add file

---
+++ objs/building/ZoneActivation.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+@onready var build = $".."
+@onready var info_build = $"../ZoneBuildInfo"
+
+var is_player_in_zone: bool = false
+
+func _ready():
+	_hide_activation()
+
+
+func _process(delta):
+	if is_player_in_zone && _is_state():
+		if Input.is_action_pressed("action_e"):
+			if GP.gold >= build.price_build:
+				GP.gold -= build.price_build
+				build.state_build = build.STATE_BUILD.WORK
+				build.health = build.max_health
+				build.emit_signal('build_zone')
+
+func _on_body_entered(body):
+	is_player_in_zone = true
+	if _is_state():
+		_show_activation()
+
+func _on_body_exited(body):
+	is_player_in_zone = false
+	_hide_activation()
+		
+func _show_activation():
+	info_build.show()
+		
+func _hide_activation():
+	info_build.hide()
+	
+func _is_state() -> bool:
+	for st in build.state_activation:
+		if st == build.state_build:
+			return true
+	return false
+
+
+func _on_building_crash_build():
+	if _is_state():
+		_show_activation()
+	else:
+		_hide_activation()
